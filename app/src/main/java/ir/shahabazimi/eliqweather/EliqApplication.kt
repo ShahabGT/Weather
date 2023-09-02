73ddef84d2f24919e9e547dc76ddc0e2ed0fec21
Shahab Azimi <azimishahab@gmail.com>
2023-09-02 17:34:53 +0330

    Added Dagger Dependency

---
+++ app/src/main/java/ir/shahabazimi/eliqweather/EliqApplication.kt
@@ -0,0 +1,12 @@
+package ir.shahabazimi.eliqweather
+
+import ir.shahabazimi.eliqweather.di.DaggerApplication
+
+/**
+ * @Author: Shahab Azimi
+ * @Date: 2023 - 09 - 02
+ **/
+class EliqApplication : DaggerApplication() {
+
+
+}
---
+++ app/src/main/java/ir/shahabazimi/eliqweather/di/DaggerApplication.kt
@@ -0,0 +1,10 @@
+package ir.shahabazimi.eliqweather.di
+
+import android.app.Application
+
+/**
+ * @Author: Shahab Azimi
+ * @Date: 2023 - 09 - 02
+ **/
+open class DaggerApplication : Application() {
+}
