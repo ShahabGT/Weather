1736a7918c7acdd146a9b91e294678b228e1e5ed
Shahab Azimi <azimishahab@gmail.com>
2025-05-21 09:02:01 +0330

    Update libraries and support android 15

--- app/src/main/java/ir/shahabazimi/weather/presentation/MainActivity.kt
+++ app/src/main/java/ir/shahabazimi/weather/presentation/MainActivity.kt
@@ -1,14 +1,27 @@
 package ir.shahabazimi.weather.presentation
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.insets.GradientProtection
+import androidx.core.view.insets.ProtectionLayout
 import ir.shahabazimi.weather.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        findViewById<ProtectionLayout>(R.id.mainProtection).setProtections(
+            listOf(
+                GradientProtection(
+                    WindowInsetsCompat.Side.TOP,
+                )
+            )
+        )
     }
 
 }
